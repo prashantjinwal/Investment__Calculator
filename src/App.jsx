50af14a5080d07d50ab59d6cce3398b6f86cbd2c
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-05-08 23:30:11 +0530

    Add style and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import Header from "./components/Header"
+import UserInput from "./components/UserInput"
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
+    <div>
+      <Header/>
+      <UserInput/>
+    </div>
   )
 }
 
